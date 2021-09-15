b050a7073514bb88c3d47d0c12c179086dfe2730
Zachary Kane <43886828+ZKane27@users.noreply.github.com>
2021-09-15 00:50:40 -0500

    Create order details.sql

---
+++ order details.sql
@@ -0,0 +1,19 @@
+-- Order Details
+-- Find order details made by Jill and Eva.
+-- Consider the Jill and Eva as first names of customers.
+-- Output the order date, details and cost along with the first name.
+-- Order records based on the customer id in ascending order
+
+-- Inner Join on customer id to combine the customer info with order info
+-- Grab order first name, date, details, cost using a where and in statements
+
+SELECT
+    c.first_name,
+    o.order_date,
+    o.order_details,
+    o.total_order_cost
+FROM orders o
+JOIN customers c
+ON o.cust_id=c.id
+WHERE c.first_name IN ('Jill', 'Eva')
+ORDER BY o.cust_id
