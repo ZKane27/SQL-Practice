1894ed48179b004cfccfceabc2e02cd805ce1be6
Zachary Kane <43886828+ZKane27@users.noreply.github.com>
2021-09-15 00:55:49 -0500

    Update Monthly Percentage Difference.sql

--- Monthly Percentage Difference.sql
+++ Monthly Percentage Difference.sql
@@ -2,14 +2,14 @@
 -- Given a table of purchases by date, calculate the month-over-month percentage change in revenue. The output should include the year-month date (YYYY-MM) and percentage change, rounded to the 2nd decimal point, and sorted from the beginning of the year to the end of the year.
 -- The percentage change column will be populated from the 2nd month forward and can be calculated as ((this month's revenue - last month's revenue) / last month's revenue)*100.
 
-- calculate month-over-month percentage -- YYYY-MM and percentage change as output -- rounded tot 2nd decimal point -- sorted from the beginning of the year
+-- calculate month-over-month percentage -- YYYY-MM and percentage change as output -- rounded tot 2nd decimal point -- sorted from the beginning of the year
 
 -- approach: -- group by month -- lag function to get previous year
 
-- 1. Formate date to YYYY-MM
+-- 1. Formate date to YYYY-MM
 	-- cast column to date dtype
 	-- formate date using to_char()
-- 2. Calculate month-over-month percentage change in revenue
+-- 2. Calculate month-over-month percentage change in revenue
 	-- formula: % change
 	-- 2a. calculate the total revenue of the current onth (or day's value)
 	-- 2b. Calculate last month's revenue
